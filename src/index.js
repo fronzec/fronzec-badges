c2edeb7171e9364b02ef58eff9173c18addf8e72
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2019-04-03 00:57:23 -0600

    new: agregando un archivo css global para nuestro proyecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';// Siempre que usemos JSX debemos importar React
 import ReactDOM from 'react-dom';// ReactDOM lo usaremos para renderizar componentes de React
 import Badge from './components/Badge';// Asi podemos importar un componente custom
+import './global.css';
 
 const container = document.getElementById('app');
 ReactDOM.render(<Badge/>, container);
